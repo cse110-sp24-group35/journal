10f136d4c2959a0651d7b10d8fc50bd355f3eeef
mbizzigotti <mbizzigotti@ucsd.edu>
2024-05-25 11:35:02 -0700

    Try style hack

--- src/scripts/journal-editor.js
+++ src/scripts/journal-editor.js
@@ -1,52 +1,122 @@
+const USE_SHADOW_ROOT = 0;
+
 class JournalEditor extends HTMLElement {
     constructor() {
         super(); // Inherit everything from HTMLElement
 
-        //const shadow = this.attachShadow({ mode: "open" });
-        //const input = document.createElement("input");
-        //input.type = "textarea";
+        if (USE_SHADOW_ROOT) {
+            this.attachShadow({ mode: "open" });
+            this.hack();
+        }
+        const shadow = (USE_SHADOW_ROOT ? this.shadowRoot : this);
+
+        shadow.innerHTML = `
+        <nav>
+            <input id="journalTitle" type="text" placeholder="Title" autofocus/>
+            <input id="journalDeadline" type="datetime-local"/>
+            <input id="journalTags" type="text" placeholder="Tags"/>
+            <button>Toggle Side View</button>
+            <section id="journalContent">
+                <div id="markdownEditor"></div>
+            </section>
+        </nav>
+        `
+
+        const script = document.createElement('script');
+        script.src = "https://www.unpkg.com/wysimark-standalone/dist/javascript/index.cjs.js";
+        script.onload = () => { this.setupMarkdownEditor(); };
+        shadow.appendChild(script);
+
         const style = document.createElement("style");
         style.innerHTML = `
         nav {
             display: flex;
+            gap: 0.5rem;
             flex-direction: column;
         }
-        `;
-
-        this.innerHTML = `
-            <nav>
-                <label>
-                Entry Title
-                <input type="text"/>
-                </label>
-                
-                <label>
-                Deadline
-                <input type="datetime"/>
-                </label>
-            
-                <label>
-                Tags
-                <input type="datetime"/>
-                </label>
-                
-                <label>
-                <button>Toggle Side View</button>
-                </label>
-            </nav>
-                
-            <div id="markdown-editor"></div>
-            `
-
-        this.appendChild(style);
         
-        const container = document.getElementById("markdown-editor")
-        // createWysimark is available on window now
-        const wysimark = createWysimark(container, {
-            initialMarkdown: `# Heading 1
-## Heading 2
-hello world`,
+        #journalTitle {
+            font-size: 2.5rem;
+        }
+
+        #journalTags, #journalDeadline {
+            font-size: 1.0rem;
+        }
+        
+        #journalContent {
+            display: flex;
+        }
+        
+        #markdownEditor {
+            width: 100%;
+        }
+        
+        button {
+            width: fit-content;
+            align-self: end;
+        }
+        `
+        shadow.appendChild(style);
+    }
+
+    setupMarkdownEditor() {
+        const container = (USE_SHADOW_ROOT ? this.shadowRoot : document)
+            .getElementById('markdownEditor');
+
+        this.wysimark = createWysimark(container, {
+            initialMarkdown: '# Heading 1\n## Heading 2\nhello world',
+            onChange: (markdown) => {
+                if (this.sideBySide) {
+                    const textarea = document.querySelector('textarea');
+                    textarea.value = markdown;
+                }
+            },
+        });
+    }
+
+    connectedCallback() {
+        // Side View Button functionality
+        const shadow = (USE_SHADOW_ROOT ? this.shadowRoot : document);
+        const button = shadow.querySelector('button');
+        button.addEventListener('click', () => {
+            const content = shadow.getElementById('journalContent');
+            const editor = shadow.getElementById('markdownEditor');
+            if (this.sideBySide) {
+                content.removeChild(shadow.querySelector('textarea'));
+                editor.style.width = "100%";
+            } else {
+                const textarea = document.createElement('textarea');
+                textarea.value = this.wysimark.getMarkdown();
+                textarea.addEventListener('input', () => {
+                    this.wysimark.setMarkdown(textarea.value);
+                });
+                content.appendChild(textarea);
+                editor.style.width = "50%";
+                textarea.style.width = "50%";
+            }
+            this.sideBySide = !this.sideBySide;
+        });
+    }
+
+    hack() {
+        this.styleObserver = new MutationObserver((mutations) => {
+            mutations.forEach((mutation) => {
+                if (mutation.type === "childList") {
+                    mutation.addedNodes.forEach(node => {
+                        if (node?.getAttribute?.('data-emotion')) {
+                            this.shadowRoot.appendChild(node.cloneNode(true));
+                        }
+                    })
+                }
+            })
         })
+
+        this.styleObserver.observe(document.head, {
+            attributes: true,
+            childList: true,
+            subtree: false,
+            attributeFilter: ['data-emotion']
+        });
     }
 }
 
