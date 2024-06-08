8cf424d596ff2dbf76bef88ffcfdcf6618efafa1
Shota Yasuraoka <yshota296@gmail.com>
2024-06-07 20:14:05 -0700

    fix timezone issue

---
+++ jest.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+module.exports = {
+    testTimeout: 600000,
+    transform: {},
+    collectCoverageFrom: [
+        'src/scripts/**/*.{js,jsx}',
+        '!src/scripts/components/**/*.js',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90
+        },
+    },
+};
