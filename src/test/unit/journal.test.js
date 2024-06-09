89006f627d89921a0100e1ab68028aa08cd4e4c8
nade <i@nade.me>
2024-06-09 12:33:07 -0700

    Add unit test for removing slash logic

--- src/test/unit/journal.test.js
+++ src/test/unit/journal.test.js
@@ -26,6 +26,30 @@ describe("Journal Stores", () => {
         ).to.throw()
     });
 
+    it("Remove leading slashes from path", () => {
+        createJournal("Hello World", "/hello/world", "Hello World", ["Hello", "World"]);
+
+        const content = journals.get();
+
+        expect(content.length).to.equal(1);
+
+        const journal = content[0];
+        expect(journal.path).to.equal("hello/world");
+
+        createJournal("Another Journal", "///another/path", "Content", ["Tag1", "Tag2"]);
+
+        const anotherContent = journals.get();
+        expect(anotherContent.length).to.equal(2);
+
+        const anotherJournal = anotherContent[1];
+        expect(anotherJournal.path).to.equal("another/path");
+    });
+
+    it("Throw error if path is invalid after removing slashes", () => {
+        expect(() => createJournal("Invalid Path Journal", "////", "Content", ["Tag1", "Tag2"]))
+            .to.throw(Error, "The path is not valid!");
+    });
+
     it("Get valid journal", () => {
         const journal = getJournal("hello/world");
 
