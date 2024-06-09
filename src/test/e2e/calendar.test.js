c6b59af5d563d09b248eef24225fdcba45da6025
ethanlee7102 <ethanlee7102@gmail.com>
2024-06-08 22:26:40 -0700

    e2e fix?

--- src/test/e2e/calendar.test.js
+++ src/test/e2e/calendar.test.js
@@ -113,7 +113,7 @@ describe("Calendar View", () => {
             helper.createTask("modalTestID", "Test Task", "Test Description", "High", "PLANNED", Date.now());
         });
 
-        let taskItem = await page.$$('.day-task-item');
+        let taskItem = await page.$$('.task-list');
         await taskItem[0].click();
 
         const modal = await page.$('task-modal');
