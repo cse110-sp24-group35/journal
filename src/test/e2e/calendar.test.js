169b407a5244a1e5cd100b6b9621c8c9769c0670
ethanlee7102 <ethanlee7102@gmail.com>
2024-06-08 23:27:52 -0700

    asdfsadfsa

--- src/test/e2e/calendar.test.js
+++ src/test/e2e/calendar.test.js
@@ -26,7 +26,7 @@ describe("Calendar View", () => {
         });
 
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             slowMo: 50,
             args: ["--no-sandbox", "--disable-setuid-sandbox"]
         });
