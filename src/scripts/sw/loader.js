3fee7984772917d351c48d892eab7e8da4ebb95f
allen <i@nade.me>
2024-06-09 21:46:25 -0700

    Ok i didn't know SW really likes to match the path letter-by-letter

--- src/scripts/sw/loader.js
+++ src/scripts/sw/loader.js
@@ -17,7 +17,7 @@ async function init() {
 
             // Failed with broader scope (likely production build), fall back to a tigher scope
             const registration = await navigator.serviceWorker.register("./worker.js", {
-                scope: "/journal",
+                scope: "/journal/",
             });
 
             if (registration.active) {
